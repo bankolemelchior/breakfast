68806988735aeaf05a2acc32f1ef4e44d4797e40
Omotosho <bankolemelchior@gmail.com>
2024-10-12 11:50:08 +0200

    wrap du boc Categorie en une méthode

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -26,79 +26,83 @@ class HomePage extends StatefulWidget {
     _getCategories();
     return Scaffold(
       appBar: appBar(),
-      // backgroundColor: Colors.white,
+      backgroundColor: Colors.white,
       body:Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           _searchField(),
           const SizedBox(height: 40),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Padding(
-                padding: EdgeInsets.only(left: 20),
-                child: Text(
-                  'Category',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 18,
-                    fontWeight: FontWeight.w600
-                  ),
-                ),
-              ),
-              const SizedBox(height: 15),
-              SizedBox(
-                height: 158,
-                child: ListView.separated(
-                  itemCount: categories.length,
-                  scrollDirection: Axis.horizontal,
-                  padding: const EdgeInsets.only(
-                    left: 20,
-                    right: 20
-                  ),
-                  separatorBuilder: (context, index) => const SizedBox(width: 25,),
-                  itemBuilder: (context, index) {
-                 
-                    return Container(
-                      width: 125,
-                      decoration: BoxDecoration(
-                        color: categories[index].boxColor,
-                        borderRadius: BorderRadius.circular(16)
-                      ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Container(
-                            width: 50,
-                            height:50,
-                            decoration: const BoxDecoration(
-                              color: Colors.white,
-                              shape: BoxShape.circle
-                            ),
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: SvgPicture.asset(categories[index].iconPath),
-                            ),
-                          ),
-                          Text(
-                            categories[index].name,
-                            style: const TextStyle(
-                              fontWeight: FontWeight.w400,
-                              color: Colors.black,
-                              fontSize: 14
-                            ),
-                          )
-                        ],
-                      ),
-                    );
-                  }
-                  ),
-              )
-            ],
-          )
+          _categoriesSection()
         ],
       ),
     );
+  }
+
+  Column _categoriesSection() {
+    return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(left: 20),
+              child: Text(
+                'Category',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600
+                ),
+              ),
+            ),
+            const SizedBox(height: 15),
+            SizedBox(
+              height: 158,
+              child: ListView.separated(
+                itemCount: categories.length,
+                scrollDirection: Axis.horizontal,
+                padding: const EdgeInsets.only(
+                  left: 20,
+                  right: 20
+                ),
+                separatorBuilder: (context, index) => const SizedBox(width: 25,),
+                itemBuilder: (context, index) {
+               
+                  return Container(
+                    width: 125,
+                    decoration: BoxDecoration(
+                      color: categories[index].boxColor,
+                      borderRadius: BorderRadius.circular(16)
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Container(
+                          width: 50,
+                          height:50,
+                          decoration: const BoxDecoration(
+                            color: Colors.white,
+                            shape: BoxShape.circle
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: SvgPicture.asset(categories[index].iconPath),
+                          ),
+                        ),
+                        Text(
+                          categories[index].name,
+                          style: const TextStyle(
+                            fontWeight: FontWeight.w400,
+                            color: Colors.black,
+                            fontSize: 14
+                          ),
+                        )
+                      ],
+                    ),
+                  );
+                }
+                ),
+            )
+          ],
+        );
   }
 
   Container _searchField() {
