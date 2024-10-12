57ced862fa20a5093359cbc21574785581ab7155
Omotosho <bankolemelchior@gmail.com>
2024-10-12 13:42:45 +0200

    wrap de la section recommendation

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -45,93 +45,97 @@ class HomePage extends StatefulWidget {
           const SizedBox(height: 40),
           _categoriesSection(),
           const SizedBox(height: 40),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-             const Padding(
-              padding: EdgeInsets.only(left: 20),
-              child: Text(
-                "Recommendation\n for Diet",
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 18,
-                  fontWeight: FontWeight.w600
-                ),
-              ),
-            ),
-            const SizedBox(height: 40),
-                  Container(
-                    height: 240,
-                    child: ListView.separated(
-                      itemCount: diets.length,
-                      itemBuilder: (context, index) {
-                        return Container(
-                          width: 210,
-                          decoration: BoxDecoration(
-                            color: diets[index].boxColor.withOpacity(0.3),
-                            borderRadius: BorderRadius.circular(20)
-                          ),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              SvgPicture.asset(diets[index].iconPath),
-                              Text(
-                                diets[index].name,
-                                style: const TextStyle(
-                                  fontWeight: FontWeight.w500,
-                                  fontSize: 16,
-                                  color: Colors.black
-                                ),
-                              ),
-                              Text(
-                                diets[index].level + ' | ' + diets[index].duration + ' | ' + diets[index].calorie,
-                                 style: const TextStyle(
-                                  fontWeight: FontWeight.w400,
-                                  fontSize: 13,
-                                  color: Color(0xff7B6F72)
-                                ),
-                              ),
-                              Container(
-                                height: 45,
-                                width: 130,
-                                decoration: BoxDecoration(
-                                  gradient: LinearGradient(
-                                   colors: [
-                                      diets[index].viewIsSelected ? const Color(0xff9DCEFF) : Colors.transparent,
-                                      diets[index].viewIsSelected ? const Color(0xff92A3FD) : Colors.transparent
-                                    ]
-                                    ),
-                                     borderRadius: BorderRadius.circular(50)
-                                ),
-                                child: Center(
-                                  child: Text(
-                                    'View',
-                                    style: TextStyle(
-                                      color: diets[index].viewIsSelected ? Colors.white : const Color(0xffC58BF2),
-                                      fontWeight: FontWeight.w600,
-                                      fontSize: 14
-                                    ),
-                                  ),
-                                ),
-                                
-                              )
-                            ],
-                          ),
-                        );
-                      },
-                      separatorBuilder: (context, index) => const SizedBox(width: 25,),
-                      scrollDirection: Axis.horizontal,
-                      padding: const EdgeInsets.only(
-                        left: 20,
-                        right: 20
-                      ),
-                  )
-                  )
-            ],
-          )
+          _recommendationSection()
         ],
       ),
     );
+  }
+
+  Column _recommendationSection() {
+    return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+           const Padding(
+            padding: EdgeInsets.only(left: 20),
+            child: Text(
+              "Recommendation\n for Diet",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 18,
+                fontWeight: FontWeight.w600
+              ),
+            ),
+          ),
+          const SizedBox(height: 40),
+                Container(
+                  height: 240,
+                  child: ListView.separated(
+                    itemCount: diets.length,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        width: 210,
+                        decoration: BoxDecoration(
+                          color: diets[index].boxColor.withOpacity(0.3),
+                          borderRadius: BorderRadius.circular(20)
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            SvgPicture.asset(diets[index].iconPath),
+                            Text(
+                              diets[index].name,
+                              style: const TextStyle(
+                                fontWeight: FontWeight.w500,
+                                fontSize: 16,
+                                color: Colors.black
+                              ),
+                            ),
+                            Text(
+                              diets[index].level + ' | ' + diets[index].duration + ' | ' + diets[index].calorie,
+                               style: const TextStyle(
+                                fontWeight: FontWeight.w400,
+                                fontSize: 13,
+                                color: Color(0xff7B6F72)
+                              ),
+                            ),
+                            Container(
+                              height: 45,
+                              width: 130,
+                              decoration: BoxDecoration(
+                                gradient: LinearGradient(
+                                 colors: [
+                                    diets[index].viewIsSelected ? const Color(0xff9DCEFF) : Colors.transparent,
+                                    diets[index].viewIsSelected ? const Color(0xff92A3FD) : Colors.transparent
+                                  ]
+                                  ),
+                                   borderRadius: BorderRadius.circular(50)
+                              ),
+                              child: Center(
+                                child: Text(
+                                  'View',
+                                  style: TextStyle(
+                                    color: diets[index].viewIsSelected ? Colors.white : const Color(0xffC58BF2),
+                                    fontWeight: FontWeight.w600,
+                                    fontSize: 14
+                                  ),
+                                ),
+                              ),
+                              
+                            )
+                          ],
+                        ),
+                      );
+                    },
+                    separatorBuilder: (context, index) => const SizedBox(width: 25,),
+                    scrollDirection: Axis.horizontal,
+                    padding: const EdgeInsets.only(
+                      left: 20,
+                      right: 20
+                    ),
+                )
+                )
+          ],
+        );
   }
 
   Column _categoriesSection() {
